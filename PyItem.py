da4bdc3b681fcad6d20b6e19a5548d6b8134ac53
ActionLuzifer <actionluzi@yahoo.de>
2013-09-23 16:51:36 +0200

    Umstrukturierung + Groessenaenderung
    Nur noch 1 Widget und eine ScrollArea
    Die Buttons werden so weit verbreitert wie die ScrollArea breit ist

--- PyItem.py
+++ PyItem.py
@@ -8,30 +8,106 @@ Created on 17.08.2013
 
 import sys
 from PyQt4 import QtCore, QtGui
-from PyQt4.QtGui import QWidget, QApplication, QLabel, QFrame, QPalette, QScrollArea
-from PyQt4.QtCore import QEvent, pyqtSignal, QObject
-import PyQt4
+from PyQt4.QtGui import QWidget, QApplication, QFrame, QPalette, QScrollArea, QCursor
+from PyQt4.QtCore import pyqtSignal, QObject
 
 
-class PyAbstractItemHandler(QScrollArea):
-    def __init__(self, _parent):
+
+class PyScrollWidget(QScrollArea):
+    def __init__(self, _parent=None):
         QScrollArea.__init__(self, _parent)
-        self.move(0,0)
-        self.buttonHeight = 20
-        self.buttonGap = 5
+        
+        # ScrollBarAlwaysOff == 1
+        # TODO: Finden wo "ScrollBarAlwaysOff" definiert ist und dann einsetzen statt der Zahl
+        self.setHorizontalScrollBarPolicy(1)
+        
+        if _parent:
+            self.setGeometry(_parent.geometry())
+        else:
+            self.setGeometry(1,1,1,1)
+
+        self.btnWidget = QWidget()
+        self.oldSizeX = 0
+        self.oldSizeY = 0
+        self.oldSizeWidth  = 1
+        self.oldSizeHeight = 1 
+        self.btnWidget.setGeometry(self.oldSizeX, self.oldSizeY, self.oldSizeWidth, self.oldSizeHeight)
+        self.setWidget(self.btnWidget)
+        
         self.scrollX = 0
         self.scrollY = 0
+        
+        self.setFrameStyle(1)
+        
+        # SIGNALS
+        self.SIGNAL_onWidthChange = 'widthchange(PyQt_PyObject)'
+
+
+    def resizeEvent(self, _resizeEvent):
+        newWidthScrollArea = self.size().width()
+        #print("oldWidth:", self.oldSizeWidth, " | newWidth:", newWidth)  
+        if newWidthScrollArea != self.oldSizeWidth:
+            print(_resizeEvent)
+            
+            if self.verticalScrollBar().isVisible():
+                newWidthBtnWidget = self.width()-self.verticalScrollBar().width()
+            else:
+                newWidthBtnWidget = self.width()
+            
+            self.btnWidget.resize(newWidthBtnWidget, self.btnWidget.height())
+            self.oldSizeWidth = self.btnWidget.size().width()
+            
+            print("SIGNAL: SIGNAL_onWidthChange")
+            self.emit(QtCore.SIGNAL(self.SIGNAL_onWidthChange), newWidthBtnWidget-3)
+        pass
+        
+        return QScrollArea.resizeEvent(self, _resizeEvent)
+
+
+    def onScroll(self):
+        self.emit(QtCore.SIGNAL(self.SIGNAL_onScroll))
+
+
+    def isButtonVisible(self, _button):
+        if ( _button.y() > self.scrollY) and (_button.y() < (self.scrollY + self.height()) ):
+            return True
+        else:
+            return False
+
+
+    def mouseMoveEvent(self, _qmouseevent):
+        self.emit(QtCore.SIGNAL(self.SIGNAL_mousemove), _qmouseevent)
+        return QWidget.mouseMoveEvent()
+
+
+    def mousePressEvent(self, _event):
+        self.emit(QtCore.SIGNAL(self.SIGNAL_mousemove), _event)
+        return QWidget.mousePressEvent(self, _event)
+
+
+    def mouseReleaseEvent(self, _event):
+        pass
+
+
+class PyAbstractItemHandler(QObject):
+
+    def __init__(self, _scrollWidget):
+        QObject.__init__(self)
+        
+        # scrollwidget erhalten und verbinden
+        self.scrollWidget = _scrollWidget
+        self.btnWidget = self.scrollWidget.btnWidget
+        QObject.connect(self.scrollWidget, QtCore.SIGNAL(self.scrollWidget.SIGNAL_onWidthChange), self.slotOnWidthChange)
+        
+        
+        
+        self.buttonHeight = 20
+        self.buttonGap = 5
         self.buttonList = []
         self.buttonListVisible = []
         self.buttonListHovered = []
         self.buttonListIsEmpty = True
         print("laenge Buttonlist:",len(self.buttonList))
-        
-        self.setFrameStyle(1)
-        
-        # sich selbst anpassen und anzeigen
-        self.resize(_parent.width(), _parent.height())
-        self.show()
 
 
     def addButton(self, _no, _title):
@@ -42,20 +118,15 @@ class PyAbstractItemHandler(QScrollArea):
         else:
             y = (len(self.buttonList))*(self.buttonHeight+self.buttonGap)
 
-        item = PyItem(self, self, _no, _title, x, y, self.width()-20, self.buttonHeight)
+        item = PyItem(self.scrollWidget.btnWidget, self, _no, _title, x, y, self.scrollWidget.width()-20, self.buttonHeight)
+        self.btnWidget.resize(self.btnWidget.width(), y+self.buttonHeight)
+        self.scrollWidget.resize(self.btnWidget.width(), y+self.buttonHeight)
         item.oY = y 
 
         self.buttonList.append(item)
         item.setFrameStyle(1)
         if self.isButtonVisible(item):
             item.show()
-
-
-    def resizeEvent(self, _resizeEvent):
-        #self.scrollbarVert.move(self.width()-self.scrollbarVert.width(), 0)
-        #self.scrollbarVert.resize(self.scrollbarVert.width(), self.height())
-        self.scrollArea.setGeometry(self.geometry())
-        QWidget.resizeEvent(self, _resizeEvent)
 
 
     def onMouseMove(self, senderEvent):
@@ -73,6 +144,9 @@ class PyAbstractItemHandler(QScrollArea):
         self.buttonListHovered = []
         sender.decorateHover()
         self.buttonListHovered.append(sender)
+        mousePos = self.scrollWidget.mapFromGlobal(QCursor.pos())
+        print(mousePos)
+        
         print("Olé")
 
 
@@ -88,7 +162,7 @@ class PyAbstractItemHandler(QScrollArea):
         print("def onMouseReleaseEvent(self, sender):")
         sender.isMousePress = False
         sender.decorate()
-        # wenn beim loslassen der Cursor über dem Button liegt, dann ist er Selected
+        # wenn beim loslassen der Cursor über dem Button liegt und nicht gescrollt wurde, dann ist er Selected
 
 
     def onScroll(self):
@@ -101,13 +175,14 @@ class PyAbstractItemHandler(QScrollArea):
 
 
     def isButtonVisible(self, _button):
-        if ( _button.y() > self.scrollY) and (_button.y() < (self.scrollY + self.height()) ):
+        if ( _button.y() > self.scrollWidget.scrollY) and (_button.y() < (self.scrollWidget.scrollY + self.scrollWidget.height()) ):
             return True
         else:
             return False
         
     def mouseMoveEvent(self, _qmouseevent):
-        #self.mouseKoordX = _qmouseevent.
+        mousePos = self.mapFromGlobal(QCursor.pos())
+        print(mousePos)
         print(_qmouseevent)
         return QWidget.mouseMoveEvent()
         
@@ -120,6 +195,11 @@ class PyAbstractItemHandler(QScrollArea):
     def mouseReleaseEvent(self, _event):
         senderEvent = (self, _event)
         return QWidget.mouseReleaseEvent(self, _event)
+
+
+    def slotOnWidthChange(self, _width):
+        for btn in self.buttonList:
+            btn.resize(_width, btn.size().height())
 
 
 
@@ -215,7 +295,8 @@ class PyAbstractItem(QFrame):
     
     def decorateMousepress(self):
         self.setBackgroundRole(QtGui.QPalette.Highlight)
-        
+
+
 
 class PyItem(PyAbstractItem):
     
--- start.py
+++ start.py
@@ -15,45 +15,38 @@ from PyQt4.QtGui import QWidget, QApplication
 import PyItem
 
 
-class myWidget(QWidget):
-
-    def __init__(self, _parent=None):
-        QWidget.__init__(self, _parent)
-        # create buttonHandler
-        self.btnHandler = PyItem.PyAbstractItemHandler(self)
-        self.btnHandler.show()
-
-    
-    def resizeEvent(self, _resizeEvent):
-        self.btnHandler.resize(self.size())
-
 if __name__ == '__main__':
     # create application
     qapp = QApplication(sys.argv)
     
     
     # create mainwindow
-    mainwidget = myWidget()
-    mainwidget.show()
+    scrollWidget = PyItem.PyScrollWidget()
+    scrollWidget.move(100,100)
+    btnHandler = PyItem.PyAbstractItemHandler(scrollWidget)
+    scrollWidget.show()
+
+    
+    
     
 
     # add Buttons
-    mainwidget.btnHandler.addButton("#01", "Titel01")
-    mainwidget.btnHandler.addButton("#02", "Titel02")
-    mainwidget.btnHandler.addButton("#03", "Titel03")
-    mainwidget.btnHandler.addButton("#04", "Titel04")
-    mainwidget.btnHandler.addButton("#05", "Titel05")
-    mainwidget.btnHandler.addButton("#06", "Titel06")
-    mainwidget.btnHandler.addButton("#07", "Titel07")
-    mainwidget.btnHandler.addButton("#08", "Titel08")
-    mainwidget.btnHandler.addButton("#09", "Titel09")
-    mainwidget.btnHandler.addButton("#10", "Titel10")
-    mainwidget.btnHandler.addButton("#11", "Titel11")
-    mainwidget.btnHandler.addButton("#12", "Titel12")
-    mainwidget.btnHandler.addButton("#13", "Titel13")
-    mainwidget.btnHandler.addButton("#14", "Titel14")
-    mainwidget.btnHandler.addButton("#15", "Titel15")
-    mainwidget.btnHandler.addButton("#16", "Titel16")
+    btnHandler.addButton("#01", "Titel01")
+    btnHandler.addButton("#02", "Titel02")
+    btnHandler.addButton("#03", "Titel03")
+    btnHandler.addButton("#04", "Titel04")
+    btnHandler.addButton("#05", "Titel05")
+    btnHandler.addButton("#06", "Titel06")
+    btnHandler.addButton("#07", "Titel07")
+    btnHandler.addButton("#08", "Titel08")
+    btnHandler.addButton("#09", "Titel09")
+    btnHandler.addButton("#10", "Titel10")
+    btnHandler.addButton("#11", "Titel11")
+    btnHandler.addButton("#12", "Titel12")
+    btnHandler.addButton("#13", "Titel13")
+    btnHandler.addButton("#14", "Titel14")
+    btnHandler.addButton("#15", "Titel15")
+    btnHandler.addButton("#16", "Titel16")
     
     # start application
     sys.exit(qapp.exec_()) 
