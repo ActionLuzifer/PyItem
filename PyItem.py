798467afe7c5977c2ad22369c28c41373fcea260
ActionLuzifer <actionluzi@yahoo.de>
2013-09-26 18:23:53 +0200

    das eingebaute abs() reicht völlig

--- PyItem.py
+++ PyItem.py
@@ -9,7 +9,6 @@ Created on 17.08.2013
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import QWidget, QFrame, QScrollArea, QCursor
 from PyQt4.QtCore import pyqtSignal, QObject, QPoint
-from numpy.ma.core import abs
 
 
 KEY_STRG = 16777249
